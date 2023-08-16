368c45a3bd9b30da4cf52f7701e40c6f669502d7
milodev <milenko.ilic18@gmail.com>
2023-08-16 16:23:47 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    screens: {
+      'sm': { 'max': '376px' },
+      // Mobile	
+
+      'md': {  'min': '377px', 'max': '959px'},
+      // Tablet & lower
+
+      'lg': { 'min': '960px' },
+      // Laptop 
+      'xl' : { 'min': '980px' },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
